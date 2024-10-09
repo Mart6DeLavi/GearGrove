522aa836a8844827db8fe36677d400835ba07ca7
Mart6DeLavi <128328679+Mart6DeLavi@users.noreply.github.com>
2024-10-09 10:30:30 +0200

    Test commit

---
+++ src/main/java/com/logitrack/geargrove/GearGroveApplication.java
@@ -0,0 +1,13 @@
+package com.logitrack.geargrove;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GearGroveApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GearGroveApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/logitrack/geargrove/GearGroveApplicationTests.java
@@ -0,0 +1,13 @@
+package com.logitrack.geargrove;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GearGroveApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
